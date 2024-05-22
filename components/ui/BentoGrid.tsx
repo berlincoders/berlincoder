805fd381cbc20c2534c19e85e1142718f83b23d9
Romeo <ralonsoad@gmail.com>
2024-05-22 23:05:33 +0200

    Card 3 Done

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -47,7 +47,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
         className
       )}
       //  Add Dynamic Style
@@ -93,9 +93,34 @@ export const BentoGridItem = ({
           <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
             {title}
           </div>
-        </div>
         {id === 2 && <GlobeDemo />}
+        {id === 3 &&(
+          <div className=" flex gab-1 ld:gab-5 w-fit absolute -right-3 ld:-right-2 ">
+            <div className="flex flex-col gap-3 ld:gap-8">
+
+              {['Ruby', 'React.js','Next.js','TypeScript'].map ((item) => (
+
+                <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bc-[#10132E]">
+                  {item}
+                </span>
+              ))}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+            </div>
+            <div className="flex flex-col gap-3 ld:gap-8">
+            <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+              {['JavaScript', 'PostgreSQL','Python','Node.js'].map ((item) => (
+
+                <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bc-[#10132E]">
+                  {item}
+                </span>
+              ))}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+            </div>
+
+          </div>
+        )}
       </div>
+    </div>
     </div>
   );
 };
