3e4b42a238c4e6e08074a00695060ae219f92cb6
Romeo <ralonsoad@gmail.com>
2024-05-23 15:24:05 +0200

    Element 6 compleated

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -71,7 +71,7 @@ const handleCopy = () => {
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      <div className ={`${id === 6} && 'flex justify-center h-full`}>
+      <div className ={`${id === 6 && 'flex justify-center'} h-full`}>
         <div className="w-full h-full absolute">
           {img &&(
             <img
@@ -100,13 +100,18 @@ const handleCopy = () => {
           titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
         )}>
 
+          {/* description &  title */}
 
-          <div className="font-sans font-bold text-lg lg:text-3xl max-w-100 z-10">
-            {title}
-          </div>
-          <div className=" mt-5 font-sans font-extralight md:max-w-100 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10 ">
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
           </div>
+          <div
+            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+          >
+            {title}
+          </div>
+
+
         {id === 2 && <GlobeDemo />}
         {id === 3 &&(
           <div className=" flex gab-1 ld:gab-5 w-fit absolute -right-3 ld:-right-2 ">
