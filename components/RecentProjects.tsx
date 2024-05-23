d449c374ebabf7cbbd935ff075bbfb8518d24eb9
Romeo <ralonsoad@gmail.com>
2024-05-23 23:56:56 +0200

    Start with Projects

---
+++ components/RecentProjects.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const RecentProjects = () => {
+  return (
+    <div className="py-20">
+      <h1 className="heading">
+        A small selection of {''}
+        <span className="text-purple"> recent projects</span>
+      </h1>
+      {/* here we will show our project cards */}
+      <div className ="flex flex-wrap ">
+
+      </div>
+
+    </div>
+  )
+}
+
+export default RecentProjects
