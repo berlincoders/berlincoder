3411d20ebfd105fe2a9a16659c76e2f3eea9d804
Romeo <ralonsoad@gmail.com>
2024-05-24 15:19:21 +0200

    Add Project Cards, and working on the Icons

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,5 +1,6 @@
 import { projects } from '@/data'
 import React from 'react'
+import { PinContainer } from './ui/3d-pin'
 
 const RecentProjects = () => {
   return (
@@ -12,8 +13,39 @@ const RecentProjects = () => {
       <div className ="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {/* mapping over the Projects */} {/* javascript-object-destructuring */}
         {projects.map(({ id, title, des, img, iconLists, link}) =>(
+
           <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:min-w-96 w-[80vw]">
-            {title}
+            <PinContainer title={title} href={link}>
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                    <img src="/bg.png" alt="bg-img" />
+                </div>
+                <img
+                  src ={img}
+                  alt ={title}
+                  className='z-10 absolute botton-0'
+                  />
+              </div>
+              <h1 className ="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {title}
+              </h1>
+
+              <p className ="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+                {des}
+              </p>
+
+              {/* Tecnologies Icons */}
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className='flex items-center'>
+                {iconLists.map((icon) =>(
+
+                  <div key={icon} className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center'style={{}}>
+                    <img  src={icon} alt={icon} className='p-2'/>
+                  </div>
+                ))}
+                </div>
+              </div>
+            </PinContainer>
           </div>
         ))}
       </div>
