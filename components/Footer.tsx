5ce338e38265991436e116d5f6bf297ea72e6875
Romeo <ralonsoad@gmail.com>
2024-07-04 12:36:24 +0200

    Change the text, hire a Junior

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,12 +5,10 @@ const Footer = () => {
     <footer className='w-full  pb-10 mb-[100px] md:mb-5' id="contact">
       <div className='flex flex-col items-center'>
         <h1 className='heading lg:max-w-[45vw]' >
-          Ready to take <span className='text-purple'>your</span> digital presence
-          to the next level?
+        Big opportunity to hire a  junior <span className='text-purple'>backend </span> developer!
         </h1>
         <p className='text-white-200 md:mt-10 my-5 text-center'>
-          Reach out today and let&apos;s discuss how I
-          can help you achieve your goals.
+        Reach out today, and we can discuss how I fit into your project.
         </p>
       </div>
     </footer>
