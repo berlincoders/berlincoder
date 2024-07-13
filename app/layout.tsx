5395e967cd9fdbff399c0ebb272c76abc3a6a762
Romeo <ralonsoad@gmail.com>
2024-07-13 19:08:02 +0200

    Backup layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
-import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,32 +19,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
         </ThemeProvider>
-        <Script
-          id="trust-logo"
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var tlJsHost = ((window.location.protocol == "https:") ? "https://secure.trust-provider.com/" : "http://www.trustlogo.com/");
-              document.write(unescape("%3Cscript src='" + tlJsHost + "trustlogo/javascript/trustlogo.js' type='text/javascript'%3E%3C/script%3E"));
-            `,
-          }}
-        />
-        <Script
-          id="trust-logo-initialize"
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              TrustLogo("https://www.positivessl.com/images/seals/positivessl_trust_seal_md_167x42.png", "POSDV", "none");
-            `,
-          }}
-        />
       </body>
     </html>
   );
